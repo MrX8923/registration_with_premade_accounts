4bfe2e1d3d18ea6dd9af7ce9a44e405d8c658856
MrX8923 <126096589+MrX8923@users.noreply.github.com>
2023-10-17 13:38:21 +0300

    done User registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 
-# Create your views here.
+from .forms import *
+
+
+class RegistrationView(FormView):
+    form_class = SingUpForm
+    template_name = 'registration/user_registration.html'
+    success_url = reverse_lazy('user_profile')
+    extra_context = {'title': 'Регистрация'}
+
+    def form_valid(self, form):
+        new_user: Profile = form.save()
+        login(self.request, new_user)
+        return super(RegistrationView, self).form_valid(form)
