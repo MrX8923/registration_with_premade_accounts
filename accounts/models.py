47cf645247e545afafbf81b6c31b9334f59c64f6
MrX8923 <126096589+MrX8923@users.noreply.github.com>
2023-10-17 14:18:43 +0300

    made unique email

--- accounts/models.py
+++ accounts/models.py
@@ -3,23 +3,26 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Profile(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+    )
     date_of_registration = models.DateField(
         auto_now_add=True,
-        verbose_name='Дата регистрации'
+        verbose_name='Дата регистрации',
     )
     bio = models.TextField(
         max_length=500,
         blank=True,
-        verbose_name='О себе'
+        verbose_name='О себе',
     )
     age = models.IntegerField(
         blank=True,
         null=True,
-        verbose_name='Возраст'
+        verbose_name='Возраст',
     )
     age_18 = models.BooleanField(
         default=True,
-        verbose_name='18+'
+        verbose_name='18+',
     )
 
     class Meta:
@@ -27,7 +30,7 @@ class Profile(AbstractUser):
         constraints = [
             models.CheckConstraint(
                 check=models.Q(age__gte=18),
-                name="age_gte_18"
+                name="age_gte_18",
             ),
         ]
 
